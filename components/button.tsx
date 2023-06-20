d9e3cf6ca42ff7c3612a210b4b67e1a6698fa4e2
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-06-20 12:38:17 +0300

    add: initial nextjs mdx files

---
+++ components/button.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Button = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <button
+      style={{
+        borderRadius: "3px",
+        border: "1px solid black",
+        color: "black",
+        padding: "0.5em 1em",
+        cursor: "pointer",
+        fontSize: "1.1em",
+      }}
+    >
+      {children}
+    </button>
+  );
+};
