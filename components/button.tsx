95225f4245099fe10fd69e2027309519b4e15ac7
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-06-21 13:18:57 +0300

    feat: add linting rules and config

--- components/button.tsx
+++ components/button.tsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React from 'react'
 
-export const Button = ({ children }: { children: React.ReactNode }) => {
+export const Button = ({ children }: { children: React.ReactNode }): JSX.Element => {
   return (
     <button
       style={{
-        borderRadius: "3px",
-        border: "1px solid black",
-        color: "black",
-        padding: "0.5em 1em",
-        cursor: "pointer",
-        fontSize: "1.1em",
+        borderRadius: '3px',
+        border: '1px solid black',
+        color: 'black',
+        padding: '0.5em 1em',
+        cursor: 'pointer',
+        fontSize: '1.1em'
       }}
     >
       {children}
     </button>
-  );
-};
+  )
+}
