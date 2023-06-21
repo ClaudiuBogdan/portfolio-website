4926947df468fc1a094ca8ec38d141ddac8e3c73
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-06-21 20:10:04 +0300

    feat: add initial e2e tests setup

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,8 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom'
+  testEnvironment: 'jest-environment-jsdom',
+  testMatch: ['**/__tests__/unit/**/*.test.(js|jsx|ts|tsx)']
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
