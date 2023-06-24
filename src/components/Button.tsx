4649ef3ddf7efa9793d15cdfaf3aca49d9238538
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-06-24 16:46:31 +0300

    fix: button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,4 @@
+import { FC, Ref } from 'react'
 import Link from 'next/link'
 import clsx from 'clsx'
 
@@ -8,7 +9,14 @@ const variantStyles = {
     'bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70',
 }
 
-export function Button({ variant = 'primary', className, href, ...props }) {
+type ButtonProps = {
+  variant?: keyof typeof variantStyles
+  className?: string
+  ref: Ref<HTMLAnchorElement>
+} & JSX.IntrinsicElements['button'] & JSX.IntrinsicElements['a']
+
+// FIXME: this component is using a bad pattern for handling the `href` prop
+export const Button: FC<ButtonProps> = ({ variant = 'primary', className, href, ...props }) => {
   className = clsx(
     'inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none',
     variantStyles[variant],
