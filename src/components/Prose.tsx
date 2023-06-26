29d2f8beca7291a05966b7e822401ddbe60c0d43
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-06-26 12:23:46 +0300

    fix: pose component

--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -1,6 +1,12 @@
+import React, { FC } from 'react'
 import clsx from 'clsx'
 
-export function Prose({ children, className }) {
+type ProseProps = {
+  className?: string
+  children: React.ReactNode
+}
+
+export const Prose: FC<ProseProps> = ({ children, className }) => {
   return (
     <div className={clsx(className, 'prose dark:prose-invert')}>{children}</div>
   )
