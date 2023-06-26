fa9d91da16b6efa9526c1e1d582000a7eaddb33c
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-06-26 13:31:12 +0300

    fix: about page types

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -12,7 +12,14 @@ import {
 } from '@/components/SocialIcons'
 import portraitImage from '@/images/portrait.jpg'
 
-function SocialLink({ className, href, children, icon: Icon }) {
+type SocialLinkProps = {
+  className?: string
+  href: string
+  children: React.ReactNode
+  icon: React.ElementType
+}
+
+function SocialLink({ className, href, children, icon: Icon }: SocialLinkProps) {
   return (
     <li className={clsx(className, 'flex')}>
       <Link
@@ -26,7 +33,7 @@ function SocialLink({ className, href, children, icon: Icon }) {
   )
 }
 
-function MailIcon(props) {
+function MailIcon(props: React.ComponentProps<'svg'>) {
   return (
     <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
       <path
