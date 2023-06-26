2e8c70312687267a187112fa80433220357ec4e4
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-06-26 13:47:09 +0300

    fix: projects page types

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -47,7 +47,7 @@ const projects = [
   },
 ]
 
-function LinkIcon(props) {
+function LinkIcon(props: React.ComponentProps<'svg'>) {
   return (
     <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
       <path
