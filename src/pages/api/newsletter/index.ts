bb0bb27dd1d401171011eab2d6fef0ff009d0d52
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-06-28 12:19:42 +0300

    feat: filter errors array in validation response
    The default behaviour is to send the error stack with the file location. This should be avoided for security reasons.

--- src/pages/api/newsletter/index.ts
+++ src/pages/api/newsletter/index.ts
@@ -1,9 +1,10 @@
-import { Body, ValidationPipe, createHandler, Post } from "next-api-decorators";
+import { Body, ValidationPipe, createHandler, Post, Catch } from "next-api-decorators";
 import { NewsletterSubscriptionInput } from "./_types";
 import { subscribeEmailToNewsletter } from "@/adapters/newsletter";
 import { successMessage } from "../_response-messages";
+import { exceptionHandler } from "../_exceptions-handler";
 
-
+@Catch(exceptionHandler)
 class NewsletterSubscriptionHandler {
   @Post()
   async subscribe(@Body(ValidationPipe) body: NewsletterSubscriptionInput) {
