22c14e5a0c7ad96eaaca926571801d997dd7ec3b
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-06-28 16:47:46 +0300

    feat: submit newsletter subscription using swr

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,10 +1,39 @@
+import { useEffect } from "react";
 import { Button } from "./Button";
 import { MailIcon } from "./Icons/MailIcon";
+import { useSubscribeToNewsletter } from "@/adapters/api";
 
 export function Newsletter() {
+
+  const {subscribe, loading, error, data} = useSubscribeToNewsletter();
+
+  console.log('loading', loading, data, error);
+  
+  const handleSubmit =  (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email") as string;
+    return subscribe({email})
+  };
+
+  useEffect(() => {
+    if (error) {
+      // TODO: show error
+      alert(error);
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (data) {
+      // TODO: show success
+      alert('success');
+    }
+  }, [data]);
+
     return (
       <form
-        action="/thank-you"
+        onSubmit={handleSubmit}
         className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
       >
         <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
@@ -17,12 +46,13 @@ export function Newsletter() {
         <div className="mt-6 flex">
           <input
             type="email"
+            name="email"
             placeholder="Email address"
             aria-label="Email address"
             required
             className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
           />
-          <Button type="submit" className="ml-4 flex-none">
+          <Button type="submit" disabled={loading} className="ml-4 flex-none">
             Join
           </Button>
         </div>
