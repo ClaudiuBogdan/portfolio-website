dfdea6cc4da0141da77d62707d55b4ff32b3befc
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-06-29 16:35:34 +0300

    feat: check email before adding it to subscription list

--- src/adapters/newsletter/mailchimp.ts
+++ src/adapters/newsletter/mailchimp.ts
@@ -1,4 +1,5 @@
 import mailchimp from '@mailchimp/mailchimp_marketing';
+import crypto from 'crypto';
 
 // TODO: create config file
 const apiKey = process.env.MAILCHIMP_API_KEY
@@ -12,6 +13,21 @@ mailchimp.setConfig({
 });
 
 export const addEmailToNewsletterList = async (email: string) => {
+    try{
+      // check if email is already subscribed or pending
+      const encryptedEmail = crypto.createHash('md5').update(email).digest("hex");
+      const response = await mailchimp.lists.getListMember(newsletterList, encryptedEmail);
+      if(response.status === 'subscribed' || response.status === 'pending') {
+        return;
+      }
+    }
+    catch(e){
+      const error = e as {status: number}
+      if( error.status === undefined || error.status !== 404){
+        throw new Error('Mailchimp API call failed')
+      }
+    }
+
     try{
       await mailchimp.lists.addListMember(newsletterList, {
         email_address: email,
