7acf5f4cac11c7595269a5b3b72294aa9d088fdf
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-06-29 16:35:34 +0300

    feat: check email before adding it to subscription list

---
+++ src/adapters/newsletter/mailchimp.test.ts
@@ -0,0 +1,19 @@
+// BEGIN: ed8c6549bwf9
+export const addEmailToNewsletterList = async (email: string) => {
+    try{
+      const response = await mailchimp.lists.getListMember(newsletterList, md5(email));
+      if(response.status === 'subscribed') {
+        console.log('Email already subscribed');
+        return;
+      }
+      await mailchimp.lists.addListMember(newsletterList, {
+        email_address: email,
+        status: 'subscribed',
+        tags: ['articles', 'portfolio', 'newsletter'],
+      });
+    }
+    catch(e){
+      throw new Error('Mailchimp API call failed')
+    }
+}
+// END: ed8c6549bwf9
