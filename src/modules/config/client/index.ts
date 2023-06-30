6e805280c11b674a6fb0632805d670ce26b5b570
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-06-30 19:53:23 +0300

    ci: add VERCEL_URL remap to use the default url from vercel env vars

--- src/modules/config/client/index.ts
+++ src/modules/config/client/index.ts
@@ -5,7 +5,10 @@ type ClientConfig = {
   baseUrl: string
 }
 
+// Remap NEXT_PUBLIC_VERCEL_URL to NEXT_PUBLIC_SITE_URL in the vercel pipeline. At the moment, is not possible to remap the in the vercel dashboard
+const vercelUrl = getEnvVar("NEXT_PUBLIC_VERCEL_URL", "") || undefined
+
 export const clientConfig: ClientConfig = {
   env: getEnvVar("NODE_ENV", "development"),
-  baseUrl: getEnvVar("NEXT_PUBLIC_SITE_URL"),
+  baseUrl: getEnvVar("NEXT_PUBLIC_SITE_URL", vercelUrl),
 }
