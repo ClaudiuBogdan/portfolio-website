9158686666a1f65e1efad7dfdb1cdb755f1fb012
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-07-04 16:33:47 +0300

    refactor: add header component text to locales

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,9 @@ import { FC, Fragment, useEffect, useRef } from "react"
 
 import { Container } from "@/components/Container"
 import avatarImage from "@/images/avatar.jpg"
+import en from "@/locales/en.json"
+
+const text = en.components.header
 
 // Write the missing prop types
 type CloseIconProps = {
@@ -172,16 +175,19 @@ const MobileNavigation: FC<MobileNavigationProps> = (props) => {
                 <CloseIcon className="h-6 w-6 text-zinc-500 dark:text-zinc-400" />
               </Popover.Button>
               <h2 className="text-sm font-medium text-zinc-600 dark:text-zinc-400">
-                Navigation
+                {text.nav.navigation}
               </h2>
             </div>
             <nav className="mt-6">
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
-                <MobileNavItem href="/about">About</MobileNavItem>
-                <MobileNavItem href="/articles">Articles</MobileNavItem>
-                <MobileNavItem href="/projects">Projects</MobileNavItem>
+                <MobileNavItem href="/about">{text.nav.about}</MobileNavItem>
+                <MobileNavItem href="/articles">
+                  {text.nav.articles}
                 </MobileNavItem>
-                <MobileNavItem href="/uses">Uses</MobileNavItem>
+                <MobileNavItem href="/projects">
+                  {text.nav.projects}
+                </MobileNavItem>
+                <MobileNavItem href="/uses">{text.nav.uses}</MobileNavItem>
               </ul>
             </nav>
           </Popover.Panel>
@@ -218,11 +224,10 @@ const DesktopNavigation: FC<DesktopNavigationProps> = (props) => {
   return (
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-        <NavItem href="/about">About</NavItem>
-        <NavItem href="/articles">Articles</NavItem>
-        <NavItem href="/projects">Projects</NavItem>
+        <NavItem href="/about">{text.nav.about}</NavItem>
+        <NavItem href="/articles">{text.nav.articles}</NavItem>
+        <NavItem href="/projects">{text.nav.projects}</NavItem>
         <NavItem href="/uses">{text.nav.uses}</NavItem>
-        <NavItem href="/uses">Uses</NavItem>
       </ul>
     </nav>
   )
