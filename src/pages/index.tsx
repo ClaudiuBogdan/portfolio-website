df31d9c690b55d622fd9c48888c6fcc225d37a60
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-07-06 14:19:16 +0300

    feat: add custom images about me

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,9 +33,11 @@ export default function Home({ articles }: { articles: TArticle[] }) {
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
             {text.title}
           </h1>
-          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            {text.aboutMe}
-          </p>
+          <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+            {text.aboutMe.map((paragraph, index) => (
+              <p key={index}>{paragraph}</p>
+            ))}
+          </div>
           <div className="mt-6 flex gap-6">
             {socialLinks.map((socialLink) => (
               <SocialLink
