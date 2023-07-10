d71bd104560a39ed7f633ad644526e990d6f9fab
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2023-07-10 16:44:18 +0300

    feat: add tags to the bottom of articles page

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -123,7 +123,6 @@ export default function ArticlesIndex({
         title="Writing on software design, company building, and the aerospace industry."
         intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
       >
-        <Tags tags={tags} onTagClick={handleTagClicked} />
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
             {articles.map((article) => (
@@ -131,6 +130,7 @@ export default function ArticlesIndex({
             ))}
           </div>
         </div>
+        <Tags tags={tags} onTagClick={handleTagClicked} className="mt-16" />
       </SimpleLayout>
     </>
   )
