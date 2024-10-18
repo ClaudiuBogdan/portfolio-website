b04802a616a08fdc3a2e8f58118f4568bfb0a763
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-10-18 17:32:50 +0300

    feat: add audio to go context article

--- next.config.mjs
+++ next.config.mjs
@@ -6,23 +6,34 @@ import remarkGfm from "remark-gfm"
 const withMDX = mdx({
   extension: /\.mdx?$/,
   options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
     remarkPlugins: [remarkGfm],
     rehypePlugins: [rehypePrism, rehypeSlug],
-    // If you use `MDXProvider`, uncomment the following line.
     providerImportSource: "@mdx-js/react",
   },
 })
 
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  // Configure pageExtensions to include md and mdx
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-  // Optionally, add any other Next.js config below
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.(wav|mp3)$/i,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            publicPath: "/_next/static/audio/",
+            outputPath: `${isServer ? "../" : ""}static/audio/`,
+            name: "[name].[ext]",
+            esModule: false,
+          },
+        },
+      ],
+    })
+
+    return config
+  },
 }
 
 // Merge MDX config with Next.js config
